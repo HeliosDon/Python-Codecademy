ff1c9597a182186bd021f8af1ab6abbc74879805
HeliosDon <100977751+HeliosDon@users.noreply.github.com>
2022-04-17 01:47:13 -0400

    Create Insurance.py
    Python Classes: Medical Insurance Project

---
+++ Insurance.py
@@ -0,0 +1,39 @@
+class Patient:
+    # add more parameters here
+  def __init__(self, name, age, sex, bmi, num_of_children, smoker):
+    self.name = name
+    self.age = age
+    self.sex = sex
+    self.bmi = bmi
+    self.num_of_children = num_of_children
+    self.smoker = smoker
+
+  def estimated_insurance_cost(self):
+    estimated_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
+
+    print(patient1.name + "’s estimated insurance costs is " + str(estimated_cost) + " dollars.")
+  
+  def updated_age(self, new_age):
+    self.age = new_age
+    print(self.name + " is now " + str(self.age) + " years old.")
+
+
+  def num_of_children(self, num_of_children):
+    self.num_of_children = new_num_of_children
+    if self.num_of_children == 1:
+      print(self.name + " has " + str(self.num_of_children) + "child.")
+    else:
+      print(self.name + " has " + str(self.num_of_children) + "children.")
+
+  def patient_profile(self): 
+    patient_information = {}
+    patient_information["Name"] = self.name
+    patient_information["Age"] = self.age
+    patient_information["Sex"] = self.sex
+    patient_information["BMI"] = self.bmi
+    patient_information["Number of Children"] = self.num_of_children
+    patient_information["Smoker"] = self.smoker
+    return patient_information
+
+
+print(patient1.patient_profile())
