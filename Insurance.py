7e75f4e57ecc916f1eeab7bea0b67d34a9be8241
HeliosDon <100977751+HeliosDon@users.noreply.github.com>
2022-04-17 15:03:55 -0400

    Update Insurance.py

--- Insurance.py
+++ Insurance.py
@@ -8,32 +8,41 @@ class Patient:
     self.num_of_children = num_of_children
     self.smoker = smoker
 
-  def estimated_insurance_cost(self):
-    estimated_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
+patient1 = Patient("John Doe", 25, 1, 22.2, 0, 0)
 
-    print(patient1.name + "’s estimated insurance costs is " + str(estimated_cost) + " dollars.")
+print(patient1.name)
+print(patient1.age)
+print(patient1.sex)
+print(patient1.bmi)
+print(patient1.num_of_children)
+print(patient1.smoker)
   
-  def updated_age(self, new_age):
-    self.age = new_age
-    print(self.name + " is now " + str(self.age) + " years old.")
+def estimated_insurance_cost(self):
+  estimated_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
+
+  print(self.name + "’s estimated insurance costs is " + str(estimated_cost) + " dollars.")
+
+def updated_age(self, new_age):
+  self.age = new_age
+  print(self.name + " is now " + str(self.age) + " years old.")
 
 
-  def num_of_children(self, num_of_children):
-    self.num_of_children = new_num_of_children
-    if self.num_of_children == 1:
-      print(self.name + " has " + str(self.num_of_children) + "child.")
-    else:
-      print(self.name + " has " + str(self.num_of_children) + "children.")
+def num_of_children(self, num_of_children):
+  self.num_of_children = new_num_of_children
+  if self.num_of_children == 1:
+    print(self.name + " has " + str(self.num_of_children) + "child.")
+  else:
+    print(self.name + " has " + str(self.num_of_children) + "children.")
 
-  def patient_profile(self): 
-    patient_information = {}
-    patient_information["Name"] = self.name
-    patient_information["Age"] = self.age
-    patient_information["Sex"] = self.sex
-    patient_information["BMI"] = self.bmi
-    patient_information["Number of Children"] = self.num_of_children
-    patient_information["Smoker"] = self.smoker
-    return patient_information
+def patient_profile(self): 
+  patient_information = {}
+  patient_information["Name"] = self.name
+  patient_information["Age"] = self.age
+  patient_information["Sex"] = self.sex
+  patient_information["BMI"] = self.bmi
+  patient_information["Number of Children"] = self.num_of_children
+  patient_information["Smoker"] = self.smoker
+  return patient_information
 
 
 print(patient1.patient_profile())
